43882945de3415c4ae893328d57549a44e0705cb
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-08 14:25:29 +0530

    chat

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,6 +2,9 @@ import { Avatar, IconButton } from "@mui/material";
 import "./Chat.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { SearchOutlined } from "@mui/icons-material";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
 
 const Chat = () => {
   return (
@@ -26,16 +29,57 @@ const Chat = () => {
 
       <div className="chat__body">
         <p className="chat__message-container">
-        <span className="chat__name">Aadarsh</span>
-        <div className="chat__message">
-          <span className='chat__message-message'>This is a message</span>
-          <span className="chat__time">{new Date().toLocaleTimeString().slice(0,5)}</span>
-        </div>
+          <span className="chat__name">Aadarsh</span>
+          <div className="chat__message">
+            <span className="chat__message-message">This is a message</span>
+            <span className="chat__time">
+              {new Date().toLocaleTimeString().slice(0, 5)}
+            </span>
+          </div>
+        </p>
+
+        <p className="chat__message-container chat__receiver">
+          <span className="chat__name">Aadarsh</span>
+          <div className="chat__message">
+            <span className="chat__message-message">This is a message</span>
+            <span className="chat__time">
+              {new Date().toLocaleTimeString().slice(0, 5)}
+            </span>
+          </div>
+        </p>
+
+        <p className="chat__message-container">
+          <span className="chat__name">Aadarsh</span>
+          <div className="chat__message">
+            <span className="chat__message-message">This is a message</span>
+            <span className="chat__time">
+              {new Date().toLocaleTimeString().slice(0, 5)}
+            </span>
+          </div>
         </p>
       </div>
 
       <div className="chat__input-container">
-        adfasf
+        <IconButton>
+          <InsertEmoticonIcon sx={{ color: "#AEBAC1" }} />
+        </IconButton>
+        <IconButton>
+          <AttachFileIcon
+            sx={{ color: "#AEBAC1", transform: "rotate(45deg)" }}
+          />
+        </IconButton>
+        <form>
+          <input
+            type="text"
+            placeholder="Type a message"
+            className="message__input"
+          />
+          <button type="submit">send message</button>
+        </form>
+
+        <IconButton>
+          <KeyboardVoiceIcon sx={{ color: "#AEBAC1" }} />
+        </IconButton>
       </div>
     </div>
   );
